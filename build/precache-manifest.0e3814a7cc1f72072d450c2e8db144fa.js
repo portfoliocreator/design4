b633e26a2530dacef75e36c1b301979bc1223511
Aryan Gupta <aryan@Aryans-MacBook-Air.local>
2020-07-01 14:29:28 +0530

    Updated build

---
+++ build/precache-manifest.0e3814a7cc1f72072d450c2e8db144fa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef08d79c054d099191755d4d47deb51f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5468b773bbc6aef0ea0",
+    "url": "/static/css/main.924e1f19.chunk.css"
+  },
+  {
+    "revision": "6723171c127f57131a99",
+    "url": "/static/js/2.b91daee3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b91daee3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5468b773bbc6aef0ea0",
+    "url": "/static/js/main.7d382565.chunk.js"
+  },
+  {
+    "revision": "c5a17faaefd1181082a5",
+    "url": "/static/js/runtime-main.d46950a8.js"
+  },
+  {
+    "revision": "fde3c0b1ed24a390fd6180981c7439c8",
+    "url": "/static/media/award.fde3c0b1.jpg"
+  },
+  {
+    "revision": "73e3ab59751b8dd4bf5c6c8be3335e21",
+    "url": "/static/media/language.73e3ab59.png"
+  },
+  {
+    "revision": "9e5663bc5a0db818f2eaf47a2567f092",
+    "url": "/static/media/publish.9e5663bc.jpg"
+  },
+  {
+    "revision": "756b31836ca3dfc446be2b22b4962be9",
+    "url": "/static/media/skill.756b3183.jpg"
+  }
+]);
